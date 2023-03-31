dce4f1c689d01446abd7ef3a530189fae7de7b6e
sawailmandhan <sawailmandhan1@gmail.com>
2023-03-31 19:37:09 +0530

    v4

---
+++ problem1.py
@@ -0,0 +1,8 @@
+def clinic():
+  #print receipt of the patient using python programming
+    print("Melanie Dental Clinic")
+    name=input("Enter patient's name: ")
+    cleaning=input("Was cleaning performed (y/n): ")
+    cavity=input("Was cavity-filling performed (y/n): ")
+    xray=input("Was X-Ray performed (y/n): ")
+    total=0
